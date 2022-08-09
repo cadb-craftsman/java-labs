79deb2759c1c471156311bc9a26ac961b14ed019
carlos <cadb.craftsman@gmail.com>
2022-08-09 16:39:06 +0200

    Nueva clase Student.

---
+++ Basics/Student.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+/**
+ * Student
+ */
+public class Student extends Person{
+
+    private String id;
+    private String carrer;
+
+    public List<String> study(List<String> materias){
+        return null;
+    }
+
+    public List<String> learn(List<String> skills){
+        return null;
+    }
+
+}
